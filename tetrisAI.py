77d8182ecae4c1849fc173fcf8f49348b68597f9
RioT1133 <44975245+RioT1133@users.noreply.github.com>
2021-06-24 20:23:40 +0300

    lisasin tetris.py faili algse suhtlusviisi tetrisAI.pyga

--- tetrisAI.py
+++ tetrisAI.py
@@ -4,8 +4,8 @@ import random
 class gameHandler:
         
     def __init__(self, piece):
-        self.desiredX = random.randint(BOARDWIDTH)
-        self.desiredRot = random.randint(4) % len(piece['shape'])
+        self.desiredX = random.randint(0, BOARDWIDTH-4)
+        self.desiredRot = random.randint(0, 4) % len(piece['shape'])+1
         self.piece = piece
 
     def movePieceToPosition(self, pieceX): # tagastab -1 kui on vaja liikuda vasakule, 1 kui on liikuda paremale ja 0 kui on jupp õiges kohas.
@@ -21,14 +21,14 @@ class gameHandler:
         # nelja võimaliku rotatsiooni puhul on võimalik alati tahetud rotatsioonini kahe pöördega saada
         # kolme ja vähema puhul kulub selleks ainult üks
         elif self.desiredRot - pieceRotation < 3:
-            return abs(self.desiredRot - pieceRotation) / (self.desiredRot - pieceRotation)
+            return int(abs(self.desiredRot - pieceRotation) / (self.desiredRot - pieceRotation))
         else:
-            return abs(self.desiredRot - pieceRotation) / -(self.desiredRot - pieceRotation)
+            return int(abs(self.desiredRot - pieceRotation) / -(self.desiredRot - pieceRotation))
     def setDesiredX(self):
-        self.desiredX = random.randint(BOARDWIDTH) #selle leiab hiljem AIga
-        #               ^ (todo) siit tuleb lahutada jupi laius muidu võib jupp mängualalt välja minna
+        self.desiredX = random.randint(0, BOARDWIDTH-4) #selle leiab hiljem AIga
+        #               ^ lahutan vägivaldselt 4 siit, selle muudab niikuinii pärast ära kui AI implementida
     def setDesiredRot(self):
-        self.desiredRot = random.randint(4) % len(self.piece['shape']) # leitud on jääk, juhul kui jupil on vähem kui 4 võimalikku rotatsiooni
+        self.desiredRot = random.randint(0, 4) % len(self.piece['shape']) # leitud on jääk, juhul kui jupil on vähem kui 4 võimalikku rotatsiooni
     def newPiece(self, newPiece):
         self.setDesiredX()
         self.setDesiredRot()
